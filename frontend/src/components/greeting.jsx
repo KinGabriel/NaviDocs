d5a3d04affdb7319f2d54e4565aba098b78ce830
nchjhy <njhoyescano@gmail.com>
2025-06-17 00:50:17 +0800

    made some modifications with the color and paddings

--- frontend/src/components/greeting.jsx
+++ frontend/src/components/greeting.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Greeting({ name }) {
   return (
-    <div className="px-1 pt-6">
+    <div className="px-1 pt-2">
       <h2 className="text-4xl font-bold text-[#003DA5]">
         Welcome back, {name}!
       </h2>
--- frontend/src/components/statcard.jsx
+++ frontend/src/components/statcard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function StatCard({ title, value }) {
   return (
-    <div className="bg-white shadow rounded-xl p-6 flex-1 min-w-[160px] text-center">
-      <p className="text-m text-gray-500">{title}</p>
+    <div className="bg-[#ECEEF6] shadow rounded-xl p-6 flex-1 min-w-[160px] text-center">
+      <p className="text-m text-[#717171]">{title}</p>
       <p className="text-3xl font-bold text-gray-800">{value}</p>
     </div>
   );
