1f6126c58561255c604c5241c4776e2f0705b917
nchjhy <njhoyescano@gmail.com>
2025-06-17 00:52:00 +0800

    added the table title and view all btn

--- frontend/src/pages/admin/adminDashboard.jsx
+++ frontend/src/pages/admin/adminDashboard.jsx
@@ -14,7 +14,7 @@ export default function AdminDashboard() {
     { title: 'School Dean', value: 4 },
     { title: 'Department Head', value: 6 },
     { title: 'Faculty Members', value: 206 },
-    { title: 'Students', value: '12,546' },
+    { title: 'Total Users', value: '12,546' },
   ];
 
   const columns = [
@@ -69,7 +69,8 @@ export default function AdminDashboard() {
       {/* sidebar & content area */}
       <div className="flex flex-1">
         <Sidebar user={user} />
-        <main className="p-6 flex-1 overflow-y-auto">
+         <div className="flex-1 flex flex-col bg-white shadow pt-1 pb-4 px-8 mx-6 mt-8 rounded-xl">
+        <main className="p-8 flex-1 overflow-y-auto">
           <Greeting name={user?.name || 'Admin'} />
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
             {stats.map((stat) => (
@@ -77,11 +78,27 @@ export default function AdminDashboard() {
             ))}
           </div>
 
-          {/* table */}
-          <div className="mt-10">
-            <Table columns={columns} data={data} />
+      {/* table */}
+          <div className="mt-10 bg-[#f7faff] rounded-t-xl p-6">
+          <div className="flex items-center justify-between">
+            <div>
+              <h2 className="text-sm font-semibold tracking-widest text-gray-800 uppercase">
+                Recently Added Users
+              </h2>
+              <div className="w-30 h-0.5 bg-yellow-400 mt-1 rounded" />
+            </div>
+            <button className="bg-[#003DA5] text-white text-sm px-4 py-2 h-9 w-30 rounded-md hover:bg-[#002B7F]">
+              View All
+            </button>
           </div>
+        </div>
+
+        <div className="-mt-2">
+          <Table columns={columns} data={data} />
+        </div>
+        
         </main>
+      </div>
       </div>
     </div>
   );
