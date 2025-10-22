5c736b6a4b4399ed6872a1169898a54a4a3c64a0
nchjhy <njhoyescano@gmail.com>
2025-10-22 21:50:55 +0800

    feat: update loading spinner SVG for unpublishing and template creation

--- frontend/src/components/cards/publishedCard.jsx
+++ frontend/src/components/cards/publishedCard.jsx
@@ -367,26 +367,11 @@ export default function PublishedCard({
                     >
                       {isLoading ? (
                         <>
-                          <svg
-                            className="animate-spin h-4 w-4 text-white-600"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <circle
-                              className="opacity-25"
-                              cx="12"
-                              cy="12"
-                              r="10"
-                              stroke="currentColor"
-                              strokeWidth="4"
-                            ></circle>
-                            <path
-                              className="opacity-75"
-                              fill="currentColor"
-                              d="M4 12a8 8 0 018-8v8H4z"
-                            ></path>
-                          </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="#fff" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity="0.5"/>
+                            <path fill="#fff" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate"/>
+                            </path>
+                            </svg>
                           <span>Unpublishing...</span>
                         </>
                       ) : (
--- frontend/src/pages/publishedTemplateView.jsx
+++ frontend/src/pages/publishedTemplateView.jsx
@@ -322,32 +322,10 @@ export default function PublishedTemplateView() {
                     >
                       {isLoading ? (
                         <>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                            <circle cx="12" cy="2" r="0" fill="#fff">
-                              <animate attributeName="r" begin="0" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(45 12 12)">
-                              <animate attributeName="r" begin="0.125s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(90 12 12)">
-                              <animate attributeName="r" begin="0.25s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(135 12 12)">
-                              <animate attributeName="r" begin="0.375s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(180 12 12)">
-                              <animate attributeName="r" begin="0.5s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(225 12 12)">
-                              <animate attributeName="r" begin="0.625s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(270 12 12)">
-                              <animate attributeName="r" begin="0.75s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                            <circle cx="12" cy="2" r="0" fill="#fff" transform="rotate(315 12 12)">
-                              <animate attributeName="r" begin="0.875s" calcMode="spline" dur="1s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/>
-                            </circle>
-                          </svg>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                           <path fill="#fff" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity="0.5"/>
+                           <path fill="#fff" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate"/>
+                           </path></svg>
                           Creating...
                         </>
                       ) : (
