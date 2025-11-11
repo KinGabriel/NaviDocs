59c8f0a0c7eac39c320d2f87b099aba22a55fcc0
nchjhy <njhoyescano@gmail.com>
2025-11-12 02:02:36 +0800

    feat: added "required" indicator messages

--- frontend/src/pages/admin/adminCreateUser.jsx
+++ frontend/src/pages/admin/adminCreateUser.jsx
@@ -303,13 +303,13 @@ export default function CreateUser() {
                     <button
                       type="button"
                       onClick={handleClear}
-                      className="px-6 py-2 bg-gray-400 text-white rounded hover:bg-gray-500 w-full sm:w-auto"
+                      className="px-6 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700 w-full sm:w-auto"
                     >
                       Clear
                     </button>
                     <button
                       type="submit"
-                      className="px-6 py-2 bg-blue-700 text-white rounded hover:bg-blue-800 disabled:opacity-50 w-full sm:w-auto"
+                      className="px-6 py-2 bg-blue-700 text-white rounded-md hover:bg-blue-800 disabled:opacity-50 w-full sm:w-auto"
                       disabled={!isFormValid || loading}
                     >
                       Add User
